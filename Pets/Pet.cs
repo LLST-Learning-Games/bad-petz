b335993f7232ac2e15d4e2415582c709d6f5084a
Matthew Stevens <mstevens@llst.ca>
2025-10-05 19:34:19 -0700

    cat follows the cursor, jumps on click

--- Pets/Pet.cs
+++ Pets/Pet.cs
@@ -4,9 +4,15 @@ using System;
 public partial class Pet : Window
 {
     [Export] private AnimatedSprite2D _sprite;
-    [Export] private float _speed;
+    [Export] private AnimationPlayer _animationPlayer;
+    [Export] private string _happyAnimationKey = "Happy_React";
+    [Export] private float _speed = 1f;
+    [Export] private float _stopDistance = 1f;
+    
+    
     public override void _Ready()
     {
+        _sprite.Scale = new Vector2(2, 2);
         GetTree().GetRoot().SetTransparentBackground(true);
         this.SetTransparentBackground(true);
         DisplayServer.WindowSetFlag(DisplayServer.WindowFlags.Transparent, true);
@@ -17,7 +23,7 @@ public partial class Pet : Window
     {
         int currentFrame = _sprite.Frame;
         StringName currentAnimation = _sprite.GetAnimation();
-        Vector2 textureCentre = _sprite.SpriteFrames.GetFrameTexture(currentAnimation, currentFrame).GetSize() / 2;
+        Vector2 textureCentre = _sprite.SpriteFrames.GetFrameTexture(currentAnimation, currentFrame).GetSize();
         Vector2 position = _sprite.GlobalPosition;
         MousePassthroughPolygon =
         [
@@ -31,8 +37,32 @@ public partial class Pet : Window
     public override void _PhysicsProcess(double delta)
     {
         Vector2 mousePosition = GetMousePosition();
-        Vector2 direction = (mousePosition - _sprite.GlobalPosition).Normalized();
-        _sprite.GlobalPosition += direction * _speed;
-        SetPassthrough();
+        if(mousePosition.DistanceTo(_sprite.GlobalPosition) > _stopDistance 
+           && !_animationPlayer.IsPlaying())
+        {
+            Vector2 direction = (mousePosition - _sprite.GlobalPosition).Normalized();
+            _sprite.GlobalPosition += direction * _speed;
+
+            _sprite.FlipH = direction.X < 0;
+            _sprite.SetAnimation("walk");
+            _sprite.Play();
+            
+            SetPassthrough();
+        }
+        else if (_sprite.GetAnimation() != _happyAnimationKey)
+        {
+            _sprite.SetAnimation("default");
+        }
+    }
+
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed)
+        {
+            //_animationPlayer.Play(_happyAnimationKey);
+            
+            _sprite.SetAnimation(_happyAnimationKey);
+            _sprite.Play();
+        }
     }
 }
